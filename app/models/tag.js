a322c5196a3bc7a01da691442a23a587daefa0d7
Luis <luisinweb@gmail.com>
2015-12-19 08:44:41 -0300

    criado método bulkFindOrCreate

--- app/models/tag.js
+++ app/models/tag.js
@@ -18,6 +18,30 @@ module.exports = function(sequelize, DataTypes) {
     classMethods: {
       associate: function(models) {
         Tag.belongsToMany(models.Curso, { as: 'cursos', through: 'curso_tags', foreignKey: 'tagId' })
+      },
+      bulkFindOrCreate: function(rows, options){
+        var self = this;
+
+        var operations = [];
+
+        _.each(rows, function(data){
+          var opt = {};
+          opt.where = _.pick(data, 'nome');
+          opt.defaults = data;
+
+          _.extendOwn(opt, options);
+
+          console.log(opt);
+
+          operations.push(self.findOrCreate(opt));
+        });
+
+        return Promise.all(operations).then(function(tagsRaw){
+          var tags = _.map(tagsRaw, function(tagRaw){
+            return tagRaw[0];
+          })
+          return tags;
+        });
       }
     }
   });
