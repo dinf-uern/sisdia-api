1580745b7be9c4d586a87b780f148e67f8dc2547
Luis <luisinweb@gmail.com>
2015-12-20 16:38:26 -0300

    modificada a lógica de limpeza nos testes para turmas

--- test/integration/turmas.js
+++ test/integration/turmas.js
@@ -15,58 +15,48 @@ function omitDateFields(obj){
 }
 
 describe('/v1/turmas', function () {
-  var turmas, curso, sala;
-
-  before(function(done){
-
-    return db.sequelize.sync().then(function(){
-
-      var cursoOp = db.Curso.create({
-        nome: 'curso1',
-        cargaHoraria: 20,
-        descricao: 'some description'
-      }).then(function(result){
-        return curso = result.get({plain: true});
-      });
-
-      var salaOp = db.Sala.create({
-        nome: 'sala1'
-      }).then(function(result){
-        return sala = result.get({plain: true});
-      });
-
-      return Promise.all([salaOp, cursoOp]).then(function(related){
-
-        var operacoes = [];
-
-        for ( var i = 1; i <= 10; i++ ){
-          operacoes.push(db.Turma.create({
-            nome: 'turma' + i,
-            salaId: sala.id,
-            cursoId: curso.id
-          }))
-        }
-
-        return Promise.all(operacoes).then(function(result){
-          turmas = _.map(result, function(item){
-            return item.get();
-          })
-
-          done();
-
-          return turmas;
-        })
-      });
-
-    })
-
-  });
-
-  after(function(){
-    return db.sequelize.drop();
-  });
-
   describe('get /', function(){
+    var turmas, curso, sala;
+
+    before(function(){
+      return db.sequelize.sync({force: true}).then(function(){
+        var cursoOp = db.Curso.create({
+          nome: 'curso1',
+          cargaHoraria: 20,
+          descricao: 'some description'
+        }).then(function(result){
+          return curso = result.get({plain: true});
+        });
+
+        var salaOp = db.Sala.create({
+          nome: 'sala1'
+        }).then(function(result){
+          return sala = result.get({plain: true});
+        });
+
+        return Promise.all([salaOp, cursoOp]).then(function(related){
+
+          var operacoes = [];
+
+          for ( var i = 1; i <= 10; i++ ){
+            operacoes.push(db.Turma.create({
+              nome: 'turma' + i,
+              salaId: sala.id,
+              cursoId: curso.id
+            }))
+          }
+
+          return Promise.all(operacoes).then(function(result){
+            turmas = _.map(result, function(item){
+              return item.get();
+            })
+
+            return turmas;
+          });
+        });
+      });
+    });
+
     it('deve retornar as turmas', function (done) {
       request(app)
         .get('/v1/turmas')
@@ -142,6 +132,11 @@ describe('/v1/turmas', function () {
   })
 
   describe('post /', function(){
+
+    before(function(){
+      return db.sequelize.sync({force: true});
+    });
+
     it('deve criar uma turma', function (done) {
       var data = {
         nome: 'some name'
@@ -179,21 +174,51 @@ describe('/v1/turmas', function () {
   })
 
   describe('get /:id', function(){
+    var turma, curso, sala;
+
+    before(function(){
+      return db.sequelize.sync({force: true}).then(function(){
+        var cursoOp = db.Curso.create({
+          nome: 'curso1',
+          cargaHoraria: 20,
+          descricao: 'some description'
+        }).then(function(result){
+          return curso = result.get({plain: true});
+        });
+
+        var salaOp = db.Sala.create({
+          nome: 'sala1'
+        }).then(function(result){
+          return sala = result.get({plain: true});
+        });
+
+        return Promise.all([salaOp, cursoOp]).then(function(related){
+          return db.Turma.create({
+            nome: 'turma1',
+            salaId: sala.id,
+            cursoId: curso.id
+          }).then(function(result){
+            turma = result.get({plain: true});
+          });
+        });
+      });
+    });
+
     it('deve retornar uma turma', function (done) {
       request(app)
-        .get('/v1/turmas/' + turmas[0].id)
+        .get('/v1/turmas/' + turma.id)
         .set('Accept', 'application/json')
         .expect('Content-Type', /json/)
         .expect(200, done)
         .expect(function(res){
           res.body.should.instanceOf(Object);
-          res.body.should.have.property('id', turmas[0].id);
+          res.body.should.have.property('id', turma.id);
         })
     });
 
     it('deve ser capaz de retornar uma turma incluindo o curso e a sala', function (done) {
       request(app)
-        .get('/v1/turmas/' + turmas[0].id)
+        .get('/v1/turmas/' + turma.id)
         .query({include: JSON.stringify([
           { model: "curso" },
           { model: "sala" }
@@ -207,7 +232,6 @@ describe('/v1/turmas', function () {
           omitDateFields(res.body.sala).should.be.eql(omitDateFields(sala));
         })
     });
-
 
     it('deve informar quando uma turma inválido tentar ser retornado', function (done) {
       request(app)
@@ -223,18 +247,30 @@ describe('/v1/turmas', function () {
   })
 
   describe('put /:id', function(){
+    var turma;
+
+    before(function(){
+      return db.sequelize.sync({force: true}).then(function(){
+        return db.Turma.create({
+          nome: 'turma1'
+        }).then(function(result){
+          turma = result;
+        });
+      });
+    });
+
     it('deve ser capaz de atualizar uma turma', function (done) {
       var data = {nome: 'novo nome'};
 
       request(app)
-        .put('/v1/turmas/' + turmas[0].id)
+        .put('/v1/turmas/' + turma.id)
         .send(data)
         .set('Accept', 'application/json')
         .expect('Content-Type', /json/)
         .expect(200, done)
         .expect(function(res){
           res.body.should.instanceOf(Object);
-          res.body.should.have.property('id', turmas[0].id);
+          res.body.should.have.property('id', turma.id);
           res.body.should.have.property('nome', data.nome);
         })
     });
