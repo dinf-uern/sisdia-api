0c8e0b30ef28c2441971f445696053e65d3a9131
Luis <luisinweb@gmail.com>
2015-12-20 17:01:25 -0300

    adicionados atributos ao modelo turma

--- app/models/turma.js
+++ app/models/turma.js
@@ -1,6 +1,7 @@
 "use strict";
 
-var  _ = require('underscore');
+var _ = require('underscore'),
+    moment = require('moment');
 
 module.exports = function(sequelize, DataTypes) {
   var Turma = sequelize.define("Turma", {
@@ -12,6 +13,54 @@ module.exports = function(sequelize, DataTypes) {
           msg: 'O nome deve ser informado.'
         }
       }
+    },
+    periodoInscricoes: {
+      allowNull: false,
+      type: DataTypes.JSONB,
+      validate: {
+        terminoMaiorOuIgualAInicio: function(value){
+          if (!value.inicio || !moment(value.inicio).isValid())
+            throw new Error('Informe uma data válida para o início das inscrições!');
+
+          if (!value.termino || !moment(value.termino).isValid())
+            throw new Error('Informe uma data válida para o término das inscrições!');
+
+          if (moment(value.inicio) > moment(value.termino))
+            throw new Error('A data de início das inscrições não pode ser maior que a de término!');
+        }
+      }
+    },
+    periodoAulas: {
+      allowNull: false,
+      type: DataTypes.JSONB,
+      validate: {
+        terminoMaiorOuIgualAInicio: function(value){
+          if (!value.inicio || !moment(value.inicio).isValid())
+            throw new Error('Informe uma data válida para o início das aulas!');
+
+          if (!value.termino || !moment(value.termino).isValid())
+            throw new Error('Informe uma data válida para o término das aulas!');
+
+          if (moment(value.inicio) > moment(value.termino))
+            throw new Error('A data de início das aulas não pode ser maior que a de término!');
+        }
+      }
+    },
+    horarioAulas: {
+      allowNull: false,
+      type: DataTypes.JSONB,
+      validate: {
+        dias: function(value){
+          if (!value.dias || !(value.dias.constructor === Array) || value.dias.length <= 0)
+            throw new Error('Informe os dias de aula!');
+        },
+        horario: function(value){
+          if (!value.horaInicio || !moment(value.horaInicio).isValid())
+            throw new Error('Informe uma hora de início válilda para as aulas!');
+
+
+        }
+      }
     }
   }, {
     tableName: 'turmas',
@@ -19,6 +68,14 @@ module.exports = function(sequelize, DataTypes) {
       associate: function(models) {
         Turma.belongsTo(models.Sala, {as:'sala', foreignKey: 'salaId', targetKey: 'id'});
         Turma.belongsTo(models.Curso, {as:'curso', foreignKey: 'cursoId', targetKey: 'id'});
+      }
+    },
+    validate: {
+      dataInscricoesVsAulas: function(){
+        var self = this;
+
+        if (moment(self.periodoInscricoes.termino) > moment(self.periodoAulas.inicio))
+          throw new Error('A data de término das inscrições não pode ser maior do que a data de início das aulas!');
       }
     }
   });
