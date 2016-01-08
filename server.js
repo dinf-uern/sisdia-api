2a55b31ae53d10cc90fc6489bec196060936eaf8
Luis <luisinweb@gmail.com>
2016-01-08 10:49:23 -0300

    pequenos ajustes

--- server.js
+++ server.js
@@ -4,15 +4,14 @@ var express = require('express'),
 
 var app = express();
 
-require('./config/express')(app, config);
+var port = process.env.PORT || config.port;
 
-var port = (process.env.VCAP_APP_PORT || config.port);
-var host = (process.env.VCAP_APP_HOST || 'localhost');
+require('./config/express')(app, config);
 
 db.sequelize
   .sync({force: config.dropTables || false})
   .then(function () {
-    app.listen(port, host, function () {
+    app.listen(port, function () {
       console.log('Escutando na porta ' + port);
     });
   }).catch(function (e) {
